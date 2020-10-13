aa9a08657b110aea60865bf3f1793befec2d42c8
karen <karenskw@qq.com>
2020-10-13 17:03:38 +0800

    ribbon实战

--- microservice-consumer-movie-ribbon/src/main/java/com/skw/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/skw/microserviceconsumermovie/controller/MovieController.java
@@ -2,9 +2,13 @@ package com.skw.microserviceconsumermovie.controller;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.skw.microserviceconsumermovie.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +19,25 @@ import java.util.List;
 
 @RestController
 public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private RestTemplate restTemplate;
-
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
+
+
+
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
-        return this.restTemplate.getForObject("http://localhost:8080/"+id,User.class);
+        return this.restTemplate.getForObject("http://microservice-provider-user/"+id,User.class);
     }
 
-    /**
-     * 查询microservice-provider-user服务信息并返回
-     * @return microservice-provider-user服务的信息
-     */
-    @GetMapping("/user-instance")
-    public List<ServiceInstance> showInfo(){
-        return this.discoveryClient.getInstances("microservice-provider-user");
+    @GetMapping("/log-instance")
+    public void logUserInstance(){
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        MovieController.LOGGER.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
     }
+
+
 }
